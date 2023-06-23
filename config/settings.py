88676f5d8d7071464bd97727383fbf9b5d7e30b5
firuz odinaev <odinaefftut@gmail.com>
2023-06-23 15:00:07 +0500

    2.0

--- config/settings.py
+++ config/settings.py
@@ -72,10 +72,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': "railway",
+       'USER': 'postgres',
+       'PASSWORD': '5KrWX7ES6By5WWlcA7kC',
+       'HOST': 'containers-us-west-172.railway.app',
+       'PORT': '7457',
+   }
 }
 
 
